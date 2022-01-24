aab2a6dfe977594b029d696c633774a7d43dc92f
Edith <edith.chevallier@hotmail.fr>
2022-01-24 21:34:19 +0100

    set up first draft of Molkky

--- pages/team-review.vue
+++ pages/team-review.vue
@@ -21,12 +21,11 @@
     <div v-if="isOrderTeamVisible">
       <div v-for="(team, index) in teamNames" :key="index">
         <b-field :label="(index + 1).toString()">
-          <b-select placeholder="Select a name">
+          <b-select placeholder="Select a name" v-model="newOrderTeam[index]">
             <option
               v-for="(option, index) in teamNames"
               :value="option"
               :key="index"
-              @input="getSelectedOption(option)"
             >
               {{ option }}
             </option>
@@ -37,6 +36,19 @@
     <b-button class="container__buttons" expanded type="is-primary"
       >Start Game</b-button
     >
+
+    <template>
+      <div class="container__molkky">
+        <img
+          v-for="index in 12"
+          :key="index"
+          :class="`container__logo__${index}`"
+          src="../assets/vibrator-sex-pngrepo-com.png"
+          alt="Play Button"
+          v-animate-css="'tada'"
+        />
+      </div>
+    </template>
   </div>
 </template>
 
@@ -56,7 +68,7 @@ export default {
       newOrderTeam: [],
       isOrderTeamVisible: false,
     });
-
+    const dildoAmount = {};
     const orderTeams = () =>
       (state.isOrderTeamVisible = !state.isOrderTeamVisible);
 
@@ -64,14 +76,9 @@ export default {
       () => (state.teamNames = localStorage.getItem("teams").split(","))
     );
 
-    function getSelectedOption(option) {
-      console.log("selected option", option);
-    }
-
     return {
       ...toRefs(state),
       orderTeams,
-      getSelectedOption,
     };
   },
 };
@@ -90,6 +97,61 @@ export default {
   }
   &__buttons {
     margin: 10px 0;
+  }
+
+  img {
+    width: 70px;
+    height: 70px;
+    background-image: url("../assets/Yd66eUm-pink-clipart.png");
+    transform: rotate(-45deg);
+  }
+
+  &__logo__1 {
+    grid-area: logo__1;
+  }
+  &__logo__2 {
+    grid-area: logo__2;
+  }
+  &__logo__3 {
+    grid-area: logo__3;
+  }
+  &__logo__4 {
+    grid-area: logo__4;
+  }
+  &__logo__5 {
+    grid-area: logo__5;
+  }
+  &__logo__6 {
+    grid-area: logo__6;
+  }
+  &__logo__7 {
+    grid-area: logo__7;
+  }
+  &__logo__8 {
+    grid-area: logo__8;
+  }
+  &__logo__9 {
+    grid-area: logo__9;
+  }
+  &__logo__10 {
+    grid-area: logo__10;
+  }
+  &__logo__11 {
+    grid-area: logo__11;
+  }
+  &__logo__12 {
+    grid-area: logo__12;
+  }
+  &__molkky {
+    margin-top: 30px;
+    display: grid;
+    grid-template-columns: repeat(7, 2fr);
+    grid-template-rows: auto;
+    grid-template-areas:
+      " .  logo__1 . logo__2  .logo__3 . "
+      "logo__4 . logo__5 . logo__6 . logo__7 "
+      " .logo__8 . logo__9 . logo__10 ."
+      ". . logo__11 . logo__12 . .";
   }
 }
 </style>
